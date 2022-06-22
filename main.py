067670341b4af5d2cd7c8ebdceb6077f93f3d17c
USER <user@dyn3187-69.wlan.ic.ac.uk>
2022-06-22 20:40:41 +0100

    skeleton more

--- main.py
+++ main.py
@@ -23,23 +23,32 @@ window.close()
 
 def import_csvs(filepath):
     """
-    return model_df, 
+    take in filepath or api call, and create dataframes relevant for different models
+    return model_df, model
     """
     pass
 
 def run_model(dataframe):
+    """
+    run model on data to predict future outcomes
+    return model_output
+    """
     pass
 
-def display_info(dataframe):
+def display_info(model_output, df):
+    """
+    display model graph through simple gui
+    """
     pass
 
 if __name__ == '__main__':
     # ON RUNNING APPLICATION, DATA ABOUT HOUSEHOLD IS IMPORTED FROM 3 CSVs AND USED TO RUN MODEL LIVE
-    import_csvs(filepath)
+    import_csvs('data/')
     run_model(dataframe)
 
     # USER IS SHOWN GRAPH OF PAST ENERGY USE AND PREDICTED FUTURE ENERGY USE,
-    # AS WELL AS THE ESTIMATED WAYS THEY USED ENERGY
-    def 
+    # AS WELL AS THE ESTIMATED WAYS THEY USED ENERGY IN THE LAST MONTH
+
+    display_info(model_output, df)
 
     # USER IS THEN PROMPTED 
