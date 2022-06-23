1b21964ad4f7b8ca4d0c1592b60b5c3baed51ac0
matthew collins <matthew.collins21@imperial.ac.uk>
2022-06-23 10:02:01 +0100

    proof of concept

--- estimate_appliance_use.py
+++ estimate_appliance_use.py
@@ -1,15 +1,72 @@
+from cv2 import CAP_PROP_XI_OUTPUT_DATA_PACKING_TYPE
 import pandas as pd
 import PySimpleGUI as sg
 
 
-def import_csvs():
+def import_house_csv():
     house_stats = pd.read_csv('data/house_stats.csv').drop('LocationId', 1)
+    monthly_uses = pd.read_csv('data/monthly_uses.csv').drop('LocationID', 1)
     house_dict = dict(zip(house_stats.Property, house_stats.Value))
-    display_user_info(house_dict)
+    # display_user_info(house_dict)
     useful_h_dict = {
-        'cooking': [],
-        'fridge_freezer': []
+        'total_use' : monthly_uses.TotalWh.iloc[-1],
+        'cooking': {
+            'last_month_use': monthly_uses.cooking.iloc[-1],
+            'gas_stoves': house_dict['gas_stoves'],
+            'gas_ovens': house_dict['gas_ovens'],
+            'electric_stoves': house_dict['electric_stoves'],
+            'electric_ovens': house_dict['electric_ovens'],
+            'microwaves': house_dict['microwaves']},
+        'fridge_freezer': {
+            'last_month_use': monthly_uses.fridge_freezer.iloc[-1],
+            'fridges': house_dict['fridges'],
+            'freezers': house_dict['freezers'],
+            'fridge_freezers': house_dict['fridge_freezers']}
     }
+    return useful_h_dict
+
+
+def calculate_usage(dict):
+    cooking = dict['cooking']
+    fridge_freezer = dict['fridge_freezer']
+
+    total_cook = 0
+    cook_aps = 0
+    for ap, val in cooking.items():
+        if ap == 'gas_stoves':
+            cook_aps += int(val)
+            total_cook += 1200 * int(val)
+        if ap == 'gas_ovens':
+            cook_aps += int(val)
+            total_cook += 3000 * int(val)
+        if ap == 'electric_stoves':
+            cook_aps += int(val)
+            total_cook += 1800 * int(val)
+        if ap == 'electric_ovens':
+            cook_aps += int(val)
+            total_cook += 5000 * int(val)
+        if ap == 'microwaves':
+            cook_aps += int(val)
+            total_cook += 1000 * int(val)
+
+    total_ff = 0
+    ff_aps = 0
+    for ap, val in fridge_freezer.items():
+        if ap == 'fridges':
+            ff_aps += int(val)
+            total_ff += 100 * int(val)
+        if ap == 'freezers':
+            ff_aps += int(val)
+            total_ff += 150 * int(val)
+        if ap == 'fridge_freezers':
+            ff_aps += int(val)
+            total_ff += 200 * int(val)
+
+    avg_cook = total_cook / cook_aps
+    avg_ff = total_ff / ff_aps
+    cook_hours = cooking['last_month_use'] / avg_cook
+    ff_hours = fridge_freezer['last_month_use'] / avg_ff
+    return int(avg_cook), int(avg_ff), int(cook_hours), int(ff_hours)
 
 
 def display_user_info(dict):
@@ -43,4 +100,9 @@ def display_user_info(dict):
     window.close()
 
 
-import_csvs()
+useful_h_dict = import_house_csv()
+avg_cook, avg_ff, cook_hours, ff_hours = calculate_usage(useful_h_dict)
+print(avg_cook)
+print(avg_ff)
+print(cook_hours)
+print(ff_hours)
--- main.py
+++ main.py
@@ -1,25 +1,83 @@
 import PySimpleGUI as sg
+import matplotlib.pyplot as plt
 
-sg.theme('BluePurple')
+from estimate_appliance_use import import_house_csv, calculate_usage
 
-layout = [[sg.Text('Your typed chars appear here:'), sg.Text(size=(15,1), key='-OUTPUT-')],
-          [sg.Input(key='-IN-')],
-          [sg.Button('Show'), sg.Button('Exit')]]
 
-window = sg.Window('Pattern 2B', layout)
+def draw_plot():
+    plt.plot([0.1, 0.2, 0.5, 0.7])
+    plt.show(block=False)
 
-while True:  # Event Loop
-    event, values = window.read()
-    print(event, values)
-    print(type(event))
-    print(type(values))
-    if event == sg.WIN_CLOSED or event == 'Exit':
-        break
-    if event == 'Show':
-        # Update the "output" text element to be the value of "input" element
-        window['-OUTPUT-'].update(values['-IN-'])
 
-window.close()
+def gui_loop():
+    useful_h_dict = import_house_csv()
+    avg_cook, avg_ff, cook_hours, ff_hours = calculate_usage(useful_h_dict)
+    total_use = useful_h_dict['total_use']
+
+    sg.theme('BluePurple')
+
+    layout = [
+        [
+            sg.Text(f'Your last month\'s energy use was: {total_use}wh'),
+        ],
+        [
+            sg.Text(f'Which is {int(0.000223*total_use)}kg of CO2e'),
+        ],
+        [
+            sg.Button('Plot last month')
+        ],
+        [
+            sg.Text('From your energy use for cooking we calculate you cooked for:')
+        ],
+        [
+            sg.Text(f'{cook_hours} hours')
+        ],
+        [
+            sg.Text('From your energy use for fridge/freezer we calculate you had your fridge-freezer appliances on for:')
+        ],
+        [
+            sg.Text(f'{ff_hours} hours')
+        ],
+        [
+            sg.Text(f'Your predicted next week\'s energy use is: {int(total_use/4)}wh'),
+        ],
+        [
+            sg.Text(f'Which is {int(0.000223*total_use/4)}kg of CO2e'),
+        ],
+        [
+            sg.Button('Plot next week')
+        ],
+        [
+            sg.Text('If you cooked for 2 hours less next week, you could reduce your next week CO2e to:')
+        ],
+        [
+            sg.Text(f'{int(0.000223*(total_use/4 - avg_cook * 4))}kg')
+        ],
+        [
+            sg.Button('Plot compared forecasted energy')
+        ],
+        [
+            sg.Button('Exit')
+        ]
+    ]
+
+    window = sg.Window('Carbon Cutter', layout)
+
+    while True:  # Event Loop
+        event, values = window.read()
+        avg_cook, avg_ff, cook_hours, ff_hours = calculate_usage(useful_h_dict)
+        if event == sg.WIN_CLOSED or event == 'Exit':
+            break
+        if event == 'Plot last month':
+            draw_plot()
+        if event == 'Plot next week':
+            draw_plot()
+        if event == 'Show':
+            # Update the "output" text element to be the value of "input" element
+            window['-OUTPUT-'].update(values['-IN-'])
+
+    window.close()
+
 
 def import_csvs(filepath):
     """
@@ -28,6 +86,7 @@ def import_csvs(filepath):
     """
     pass
 
+
 def run_model(dataframe):
     """
     run model on data to predict future outcomes
@@ -35,20 +94,22 @@ def run_model(dataframe):
     """
     pass
 
+
 def display_info(model_output, df):
     """
     display model graph through simple gui
     """
     pass
 
+
 if __name__ == '__main__':
     # ON RUNNING APPLICATION, DATA ABOUT HOUSEHOLD IS IMPORTED FROM 3 CSVs AND USED TO RUN MODEL LIVE
-    import_csvs('data/')
-    run_model(dataframe)
+    df = import_csvs('data/')
+    model_output = run_model(df)
 
     # USER IS SHOWN GRAPH OF PAST ENERGY USE AND PREDICTED FUTURE ENERGY USE,
     # AS WELL AS THE ESTIMATED WAYS THEY USED ENERGY IN THE LAST MONTH
 
-    display_info(model_output, df)
+    # USER IS THEN PROMPTED
 
-    # USER IS THEN PROMPTED 
+    gui_loop()
